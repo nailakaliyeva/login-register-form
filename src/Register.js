c550e905ed5c46b1e8e129fa86a43a21b1ea809f
Naila <nailakaliyeva@gmail.com>
2021-01-29 22:55:23 +0000

    register fe

--- src/Register.js
+++ src/Register.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
-import { useMutation } from "@apollo/react-hooks"
+import 'semantic-ui-css/semantic.min.css'
 import { Form, Button } from "semantic-ui-react";
-import gql from 'graphql-tag'
+
 
 const Register = () => {
     const [values, setValues] = useState({
@@ -15,20 +15,22 @@ const Register = () => {
     const onChange = (e)=>  {
     setValues({...values, [e.target.name]: e.target.value})}
 
-    const [ addUser, { loading }] = useMutation(REGISTER_USER,{
-        update(proxy, result){
-        console.log(result)},
-        variables: values
-    })
+    // const [ addUser, { loading }] = useMutation(REGISTER_USER,{
+    //     update(proxy, result){
+    //     console.log(result)},
+    //     variables: values
+    // })
     const onSubmit = (e) => {
         e.preventDefault();
+
     }
     
     return(
-        <div>
+        <div className = "container">
             <Form onSubmit = {onSubmit} noValidate>
                 <h1>Register</h1>
                 <Form.Input required
+                    //error={{ content: 'Please enter your username', pointing: 'below' }}
                     label = "Username"
                     placeholder = "Username"            //username
                     name = "username"
@@ -45,6 +47,7 @@ const Register = () => {
                     />
 
                 <Form.Input required
+                    id='form-input-control-error-email'
                     label = "E-mail"
                     placeholder = "E-mail"              //email
                     name = "email"
@@ -66,33 +69,33 @@ const Register = () => {
                     value = {values.confirmPassword}                   //confirm Password
                     onChange = {onChange}
                     />
-                <Button type="submit"primary>Register</Button>            
+                <Button className = "btn btn-primary mt-2" type="submit"primary>Register</Button>            
             </Form>
             {/* <form class="ui form"><div class="required Input"><label>Last name</label><div class="ui input"><input type="text" placeholder="Full name"/></div></div></form> */}
         </div>
     );
 }
 
-const REGISTER_USER = gql`
-mutation register(
-    $name = String!
-    $username = String!
-    $email = String!
-    $password = String!
-    $confirmPassword = String!
+// const REGISTER_USER = gql`
+// mutation register(
+//     $name = String!
+//     $username = String!
+//     $email = String!
+//     $password = String!
+//     $confirmPassword = String!
 
-){
-    register(
-        registerInput:{
-            name: $name
-            username : $username
-            email : $email
-            password: $password
-            confirmPassword: $confirmPassword
-        }
-    ){
-        id email username created
-    }
-}`
+// ){
+//     register(
+//         registerInput:{
+//             name: $name
+//             username : $username
+//             email : $email
+//             password: $password
+//             confirmPassword: $confirmPassword
+//         }
+//     ){
+//         id email username created
+//     }
+// }`
 
 export default Register;
